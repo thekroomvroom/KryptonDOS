03a56b05695699cebf3fcbfbda5bf531e976f683
Larry <145891062+thekroomvroom@users.noreply.github.com>
2026-01-02 20:44:43 +0800

    Update kshell.c

--- shell/kshell.c
+++ shell/kshell.c
@@ -1,1 +1,11 @@
+#include "parser.h"
+#include <string.h>
 
+void shell_loop() {
+    char input[128];
+    while(1) {
+        kputs("KryptonDOS@localhost>");       // prompt
+        kgets(input, 128);     // simple input
+        parse_command(input);   // call parser (even if stub)
+    }
+}
