90b0c70cf6b70ed7272e297eea6314015ca7e40a
Larry <145891062+thekroomvroom@users.noreply.github.com>
2026-01-02 20:43:21 +0800

    Update io.c

--- kernel/io.c
+++ kernel/io.c
@@ -1,1 +1,14 @@
+#include "io.h"
 
+void kputs(const char *s) {
+    while (*s) {
+        asm volatile (
+            "mov ah, 0x0E\n"
+            "mov al, %0\n"
+            "int 0x10"
+            :
+            : "r"(*s)
+        );
+        s++;
+    }
+}
